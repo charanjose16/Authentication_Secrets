c5c4c5633e62a252e7a66b447679e6055a914164
charan jose <charanjoseph00@gmail.com>
2023-06-25 12:48:31 +0530

    final commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
  app.use(passport.initialize());
  app.use(passport.session());
 
-mongoose.connect("mongodb+srv://charanjoseph00:charansDB@cluster0.k9iozfz.mongodb.net/userDB",{useNewUrlParser:true});
+ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.set('strictQuery',false);
 const connectDB = async() => {
